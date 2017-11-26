6e2c2f74bff358e9a060e14cd5e77d7ef1fe2008
xianwei li <xil148@pitt.edu>
2017-11-25 16:01:07 -0800

    implemente image saving

--- app/src/main/java/com/xianwei/pictureframe/CameraActivity.java
+++ app/src/main/java/com/xianwei/pictureframe/CameraActivity.java
@@ -1,18 +1,29 @@
 package com.xianwei.pictureframe;
 
 import android.app.Activity;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import static android.content.ContentValues.TAG;
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
 /**
  * Created by xianwei li on 11/24/2017.
@@ -22,11 +33,17 @@ public class CameraActivity  extends Activity {
 
     private Camera mCamera;
     private CameraPreview mPreview;
+    private byte[] previewImage;
+    @BindView(R.id.button_capture)
+    ImageButton captureBtn;
+    @BindView(R.id.button_save)
+    ImageButton saveBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_preview);
+        ButterKnife.bind(this);
 
         // Create an instance of Camera
         mCamera = getCameraInstance();
@@ -35,6 +52,36 @@ public class CameraActivity  extends Activity {
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+    }
+
+    @OnClick(R.id.button_capture)
+    void takeImage(){
+        mCamera.takePicture(null,null, mPicture);
+        switchToSaveBtn();
+    }
+
+    @OnClick(R.id.button_save)
+    void saveImage(){
+        savePreviewImage();
+        finish();
+    }
+
+    private void savePreviewImage() {
+        File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+        if (pictureFile == null){
+            Log.d(TAG, "Error creating media file, check storage permissions:");
+            return;
+        }
+
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            fos.write(previewImage);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d(TAG, "Error accessing file: " + e.getMessage());
+        }
     }
 
     /** A safe way to get an instance of the Camera object. */
@@ -47,5 +94,57 @@ public class CameraActivity  extends Activity {
             // Camera is not available (in use or does not exist)
         }
         return c; // returns null if camera is unavailable
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            previewImage = data;
+        }
+    };
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "PictureFrame");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    private void switchToSaveBtn() {
+        captureBtn.setVisibility(View.INVISIBLE);
+        saveBtn.setVisibility(View.VISIBLE);
+    }
+
+    private void switchToCaptureBtn() {
+        captureBtn.setVisibility(View.VISIBLE);
+        saveBtn.setVisibility(View.INVISIBLE);
     }
 }
